24c86e1dd427efc9efbb4afa02b6dbb0d94cad18
Logunova Margarita <logunovar@gmail.com>
2022-10-25 21:34:08 +0300

    add .env

--- test/conftest.py
+++ test/conftest.py
@@ -34,14 +34,14 @@ def browser_management():
     login = os.getenv('LOGIN')
     password = os.getenv('PASSWORD')
 
-    driver = webdriver.Remote(
-        command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
-        options=options
-    )
+#    driver = webdriver.Remote(
+#        command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
+#        options=options
+#    )
 
-    #    driver = webdriver.Remote(
-    #        command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
-    #        options=options)
+    driver = webdriver.Remote(
+        command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=options)
 
     browser.config.driver = driver
 
