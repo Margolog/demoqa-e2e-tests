bcacb31b3df9a73d1a5ccecb5ff38a66efcee2fc
Logunova Margarita <logunovar@gmail.com>
2022-10-27 22:49:47 +0300

    new

--- model/controls/dropdown.py
+++ model/controls/dropdown.py
@@ -2,13 +2,7 @@ from selene import have
 from selene.support.shared import browser
 
 
-#def select(element, option):
-#    element.click()
- #   browser.all('[id^=react-select][id*=-option-]').find_by(have.exact_text(option))#.click()
- #   browser.element("#city").click()
-
 def select(element, option):
     element.click()
-    browser.all('[id^=react-select][id*=-option-]').by(
-        have.exact_text(option)
-    ).first.click()
+    browser.all('[id^=react-select][id*=-option-]').find_by(have.exact_text(option))  # .click()
+    browser.element("#city").click()
