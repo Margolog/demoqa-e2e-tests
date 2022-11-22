82e2b953136efd12a980a4f4e04de67ac312191c
Logunova Margarita <logunovar@gmail.com>
2022-11-22 19:22:22 +0300

    fix with FluentPageObjects and StepsObject

--- model/controls/dropdown.py
+++ model/controls/dropdown.py
@@ -4,6 +4,6 @@ from selene.support.shared import browser
 
 class DropDown:
     def select(self, element, option):
-        element.click()
+        element.perform(command.js.scroll_into_view).click()
         browser.all('[id^=react-select][id*=-option-]').find_by(have.exact_text(option)).click()
         return self
